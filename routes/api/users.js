e2f9bc4b7a2c874aa619841506ee7bfe0173a8bf
Arshad Hossain <arshad417hossain@gmail.com>
2022-08-17 18:32:33 +0600

    User Model and user route completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 //@route get app/users
 //access public
-router.get("/", (req, res) => res.send("user route"));
+router.post(
+  "/",
+  [
+    body("name").not().isEmpty().withMessage("Name is missing"),
+    body("email").isEmail().withMessage("Email is missing"),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("password must be at least 6 character"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("user routes is working");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+app.use(express.json());
+
 //connect the database
 connectDB();
 
@@ -12,10 +14,10 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(port, (req, res) => {
   console.log(`server is running on ${port}`);
